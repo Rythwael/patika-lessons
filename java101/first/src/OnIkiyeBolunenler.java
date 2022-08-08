707d21caf0852e18b8147ec50df10b3a188e897f
Oğuzhan <akcahanoguz@gmail.com>
2022-08-08 17:57:01 +0300

    12'ye bölünün sayıların ortalaması eklendi.

---
+++ java101/first/src/OnIkiyeBolunenler.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class OnIkiyeBolunenler {
+    public static void main(String[] args) {
+        int number,count=0,total=0;
+        double avg;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sayıyı giriniz");
+        number = input.nextInt();
+        for (int i=1;i<=number;i++){
+            if (i%12==0){
+                count++;
+                total += i;
+            }
+        }
+        avg = total/count;
+        System.out.print("12'ye bölünün sayıların ortalaması : " + avg);
+    }
+}
