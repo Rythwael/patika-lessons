14bf488dbd12e5e8aa086c8220614b52dd9e8051
Oğuzhan <akcahanoguz@gmail.com>
2022-08-09 21:12:44 +0300

    Min Max değer bulma

---
+++ java101/first/src/MinMaxDeger.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MinMaxDeger {
+    public static void main(String[] args) {
+        int n,min=0,max=0,tempMax=0,tempMin=0,count=1,curr=0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Kaç sayı gireceksiniz");
+        n = input.nextInt();
+        while (count<=n){
+            System.out.print("Sayıyı giriniz : ");
+            curr=input.nextInt();
+            count++;
+            if (curr < tempMin ){
+                if (tempMax == 0){
+                    tempMax = curr;
+                }
+                tempMin = curr;
+
+            } else if (curr > tempMax) {
+                if (tempMin == 0){
+                    tempMin = curr;
+                }
+                tempMax = curr;
+            }
+
+        }
+        min=tempMin;
+        max=tempMax;
+        System.out.println("Max : " + max + "\nMin : " + min);
+
+    }
+}
